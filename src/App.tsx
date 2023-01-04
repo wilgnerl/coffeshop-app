cc2b246a0203e93b600d91dcb5749481e19d5168
wilgnerl <wilgnerl@al.insper.edu.br>
2023-01-04 17:51:38 -0300

    chore: setting styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 function App() {
 
 	return (
-		<div>
-			<h1>Hello world</h1>
-		</div>
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+		</ThemeProvider>
 	);
 }
 
